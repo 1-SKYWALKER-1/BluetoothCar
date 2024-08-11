591560df3ebd30e787811a819f067f5b620cde04
Artem <art.samokysh@gmail.com>
2024-08-11 23:14:26 +0300

    use one bluetooth adapter for both fragments

--- app/src/main/java/com/example/bluetoothcar/BaseActivity.kt
+++ app/src/main/java/com/example/bluetoothcar/BaseActivity.kt
@@ -1,14 +1,18 @@
-package com.example.bt_def
+package com.example.bluetoothcar
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import com.example.bt_def.R
 
 class BaseActivity : AppCompatActivity(){
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_base)//bluetooth_layout
         //initRcView()
-        supportFragmentManager.beginTransaction().replace(R.id.PlaceHolder,DeviceListFragment()).commit()
+        supportFragmentManager.beginTransaction().replace(
+            R.id.PlaceHolder,
+            DeviceListFragment()
+        ).commit()
     }
 
 }
